c96b28f497fea893163c620010ccf752ff882984
CatherineZeng <yczeng@mit.edu>
2017-07-02 23:03:25 -0500

    recent changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function delContent(text){
+function replaceContent(text){
   var x = document.getElementsByClassName("_2pie");
   if (x[0] == null) return 0
   else{
@@ -13,28 +13,45 @@ function getHackerNews(){
   xhr.onreadystatechange = function() {
       if (xhr.readyState == XMLHttpRequest.DONE) {
           text = xhr.responseText
-          delContent(text)
-          return
+          replaceContent(text)
+          
       }
   }
   xhr.open('GET', 'https://news.ycombinator.com/', true);
   xhr.send(null);
+  return
 }
 
-const observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-        mutation.addedNodes.forEach(function(node) {
-            if (node.nodeType === 1) {
-                getHackerNews()
-            }
-        });
-    });
-});
+// const observer = new MutationObserver(function(mutations) {
+//     mutations.forEach(function(mutation) {
+//         mutation.addedNodes.forEach(function(node) {
+//             if (node.nodeType === 1) {
+//                 getHackerNews()
+//                 window.boo = true
+//                 observer.disconnect()
+//             }
+//         });
+//     });
+// });
+
+// clearfix
 
 var loop = setInterval(function() {
   const config = { attributes: false, childList: true, characterData: false, subtree: true }
-  observer.observe(document.body, config);
-  clearInterval(loop)
-}, 20);
+  var x = document.getElementsByClassName("_2pie")
+  if (x[0] != null){
+    if x.nextSibling.contains("clearfix"){
+      getHackerNews()
+    }
+  }
 
-alert("test")
+  //getHackerNews()
+
+  // try{
+  //observer.observe(document.body, config);
+  // }
+  // catch(err){
+  //   console.log("Wait")
+  // }
+  console.log("test")
+}, 20);
